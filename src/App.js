e90e57ade0142f79a885d5515feba4ec80cf73e5
skTaeji <sktaeji@gmail.com>
2018-09-19 19:39:47 -0700

    added function to display name of poker hand on click

--- src/App.js
+++ src/App.js
@@ -1,7 +1,71 @@
 import React, { Component } from 'react';
 import './App.css';
+var psHand = require('pokersolver').Hand;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        hand: [],
+        deck: [],
+        shaq: ''
+    }
+
+    this.genHand = this.genHand.bind(this);
+    this.genDeck = this.genDeck.bind(this);
+    this.imgName = this.imgName.bind(this);
+}
+
+genDeck() {
+    const suits = ['a', 'd', 'c', 'h'];
+    const cards = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+    const deck = [];
+
+    suits.forEach(s => {
+        cards.forEach(c => {
+            deck.push(c + s);
+        })
+    });
+
+    this.setState({ deck });
+}
+
+genHand() {
+    const hand = [];
+    const deck = Object.assign([], this.state.deck);
+    for (let i = 0; i < 5; i++) {
+        hand.push(deck.splice([Math.random()*deck.length], 1)[0]);
+    }
+    const shaq = psHand.solve(hand).name;
+
+    this.setState({ hand, deck, shaq });
+}
+
+imgName(str) {
+  const map = {
+      a: 'ace',
+      d: 'diamonds',
+      c: 'clubs',
+      s: 'spades',
+      T: '10',
+      J: 'jack',
+      Q: 'queen',
+      K: 'king',
+      A: 'ace',
+      2: '2',
+      3: '3',
+      4: '4',
+      5: '5',
+      6: '6',
+      7: '7',
+      8: '8',
+      9: '9',
+  };
+  
+  return `${map[str[0]]}_of_${map[str[1]]}.svg`;
+}
+
   render() {
     return (
       <div className="App">
@@ -9,8 +73,10 @@ class App extends Component {
           <h1 className="App-title">Dealer Deals</h1>
         </header>
         <p className="App-intro">
-          <button>Deal Me In</button>
+          <button onClick={this.genDeck}>Shuffle</button>
+          <button onClick={this.genHand}>Deal Me In</button>
         </p>
+        <div>{this.state.shaq}</div>
       </div>
     );
   }
