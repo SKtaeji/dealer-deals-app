5c53a2a4959c77b0088628db30fa2803ccd2615e
skTaeji <sktaeji@gmail.com>
2018-09-19 19:08:06 -0700

    initialized create-react-app & installed assets and packages

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Dealer Deals</h1>
+        </header>
+        <p className="App-intro">
+          <button>Deal Me In</button>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
